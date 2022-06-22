0c92e28b69089bf48e5a9d40162b4c7a68df8d1b
thiago <thiag@NOTETHIAGO>
2022-06-22 17:26:26 -0300

    Atualizaçoes Cliente

---
+++ LocadoraDeVeiculos.Dominio/ModuloCliente/PessoaFisica.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocadoraDeVeiculos.Dominio.ModuloCliente
+{
+    internal class PessoaFisica : Cliente
+    {
+        string CPF;
+        string CNH;
+
+        public PessoaFisica()
+        {
+
+        }
+    }
+}
---
+++ LocadoraDeVeiculos.Infra/ModuloCliente/RepositorioClienteEmBancoDeDados.cs
@@ -0,0 +1,13 @@
+﻿using LocadoraDeVeiculos.Dominio.ModuloCliente;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocadoraDeVeiculos.Infra.ModuloCliente
+{
+    public class RepositorioClienteEmBancoDeDados : RepositorioBaseEmBancoDeDados<Cliente, ValidadorCliente, MapeadorCliente>
+    {
+    }
+}
