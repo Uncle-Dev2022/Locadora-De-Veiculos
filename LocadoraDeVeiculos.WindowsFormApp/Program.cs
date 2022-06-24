fdfaf52af23f248ff6de7c9912970891b19b35bc
User-Wellengton <wellengtonjm@gmail.com>
2022-06-24 14:42:09 -0300

    Att

--- LocadoraDeVeiculos.WindowsFormApp/Program.cs
+++ LocadoraDeVeiculos.WindowsFormApp/Program.cs
@@ -6,7 +6,7 @@ using System.Windows.Forms;
 
 namespace LocadoraDeVeiculos.WindowsFormApp
 {
-    internal static class Program
+    public static class Program
     {
         /// <summary>
         ///  The main entry point for the application.
