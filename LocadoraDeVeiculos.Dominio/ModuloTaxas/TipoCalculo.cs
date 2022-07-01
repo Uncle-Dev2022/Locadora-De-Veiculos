3f68e8dbb38791b97158dda0c4a7cb32f91a9bd8
nysalvi <nysalvi20@gmail.com>
2022-07-01 16:37:38 -0300

    Fix: Implementando camada de aplicação e adicionando cálculo fixo e diário na Taxa

---
+++ LocadoraDeVeiculos.Dominio/ModuloTaxas/TipoCalculo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocadoraDeVeiculos.Dominio.ModuloTaxas
+{
+    public enum TipoCalculo
+    {
+        [Description("Cálculo Diário")]
+        Diario,
+
+        [Description("Cálculo Fixo")]
+        Fixo,
+
+    }
+}
