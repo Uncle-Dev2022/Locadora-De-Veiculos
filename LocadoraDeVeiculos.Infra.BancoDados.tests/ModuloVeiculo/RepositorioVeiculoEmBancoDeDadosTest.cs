49c33b1fb89832d6f2e5deac57421ad1539ff04d
User-Wellengton <wellengtonjm@gmail.com>
2022-07-15 15:31:39 -0300

    Att Teste Integração Veiculo Funcionante

--- LocadoraDeVeiculos.Infra.BancoDados.tests/ModuloVeiculo/RepositorioVeiculoEmBancoDeDadosTest.cs
+++ LocadoraDeVeiculos.Infra.BancoDados.tests/ModuloVeiculo/RepositorioVeiculoEmBancoDeDadosTest.cs
@@ -15,12 +15,37 @@ namespace LocadoraDeVeiculos.Infra.BancoDados.tests.ModuloVeiculo
     [TestClass]
     public class RepositorioVeiculoEmBancoDeDadosTest
     {
+        Veiculo veiculo;
+        Veiculo veiculo1;
+        Veiculo veiculo2;
+        GrupoDeVeiculo grupoDeVeiculo;
+
         private RepositorioVeiculoEmBancoDeDados repositorioVeiculo;
         private RepositorioGrupoDeVeiculoEmBancoDeDados repositorioGrupo;
 
         public RepositorioVeiculoEmBancoDeDadosTest()
         {
+
             DB.executarSql("DELETE FROM TBVEICULO;");
+            DB.executarSql("DELETE FROM TBGRUPOVEICULO;");
+
+
+            grupoDeVeiculo = new GrupoDeVeiculo("Economico");
+            veiculo = new Veiculo("Ford", "Ka", "Vermelho", "2020", "Gasolina", "BRA2E19", 100000, 50, new byte[] { })
+            {
+                GrupoDeVeiculo = grupoDeVeiculo,
+            };
+
+            veiculo1 = new Veiculo("Fiat", "Uno", "Bordo", "1994", "Gasolina", "BRA4E19", 250000, 50, new byte[] { })
+            {
+                GrupoDeVeiculo = grupoDeVeiculo,
+            };
+
+            veiculo2 = new Veiculo("Nissan", "Silvia", "Cinza", "1999", "Gasolina", "JPA2E19", 150000, 50, new byte[] { })
+            {
+                GrupoDeVeiculo = grupoDeVeiculo,
+            };
+
 
             repositorioVeiculo = new RepositorioVeiculoEmBancoDeDados();
             repositorioGrupo = new RepositorioGrupoDeVeiculoEmBancoDeDados();
@@ -30,32 +55,78 @@ namespace LocadoraDeVeiculos.Infra.BancoDados.tests.ModuloVeiculo
         [TestMethod]
         public void Deve_inserir_veiculo()
         {
-            //arrange
-            var grupo = NovoGrupo1();
-            var veiculo = NovoVeiculo1();
-            repositorioGrupo.Inserir(grupo);
-
-            //action
+            repositorioGrupo.Inserir(grupoDeVeiculo);
             repositorioVeiculo.Inserir(veiculo);
 
-            //assert
-            var resultado = repositorioVeiculo.SelecionarPorId(veiculo.Id);
 
-            Assert.IsNotNull(resultado);
-            Assert.AreEqual(veiculo, resultado);
+            var VeiculoEncontrado = repositorioVeiculo.SelecionarPorId(veiculo.Id);
+
+            Assert.IsNotNull(VeiculoEncontrado);
+            Assert.AreEqual(veiculo, VeiculoEncontrado);
         }
 
-        private GrupoDeVeiculo NovoGrupo1()
+        [TestMethod]
+        public void Deve_Editar_veiculo()
         {
-            var grupoDeVeiculo = new GrupoDeVeiculo("Economico");
-            return grupoDeVeiculo;
+            repositorioGrupo.Inserir(grupoDeVeiculo);
+            repositorioVeiculo.Inserir(veiculo);
+
+            veiculo.Marca = "Fiat";
+
+            repositorioVeiculo.Editar(veiculo);
+
+            var veiculoEditado = repositorioVeiculo.SelecionarPorId(veiculo.Id);
+
+
+            Assert.IsNotNull(veiculoEditado);
+
+            Assert.AreEqual(veiculo, veiculoEditado);
         }
 
-        private Veiculo NovoVeiculo1()
+        [TestMethod]
+        public void Deve_Excluir_Condutor()
         {
-            var veiculo = new Veiculo("Ford", "Ka","Vermelho", "2020", "Gasolina", "BRA2E19", 100000 , 50 , new byte[] { });
+            repositorioGrupo.Inserir(grupoDeVeiculo);
+            repositorioVeiculo.Inserir(veiculo);
 
-            return veiculo;
+            repositorioVeiculo.Excluir(veiculo);
+
+            var CondutorEncontrado = repositorioVeiculo.SelecionarPorId(veiculo.Id);
+
+            Assert.IsNull(CondutorEncontrado);
+
         }
+
+        [TestMethod]
+        public void Deve_Selecionar_Um_Condutor()
+        {
+            repositorioGrupo.Inserir(grupoDeVeiculo);
+            repositorioVeiculo.Inserir(veiculo);
+
+            var CondutorEncontrado = repositorioVeiculo.SelecionarPorId(veiculo.Id);
+
+            Assert.IsNotNull(CondutorEncontrado);
+
+            Assert.AreEqual(veiculo, CondutorEncontrado);
+        }
+
+        [TestMethod]
+        public void Deve_Selecionar_Todos_Os_Veiculos()
+        {
+            int quantidade = 3;
+
+            repositorioGrupo.Inserir(grupoDeVeiculo);
+            repositorioVeiculo.Inserir(veiculo);
+            repositorioVeiculo.Inserir(veiculo1);
+            repositorioVeiculo.Inserir(veiculo2);
+
+
+
+            var veiculos = repositorioVeiculo.SelecionarTodos();
+
+            Assert.AreEqual(quantidade, veiculos.Count);
+
+        }
+
     }
 }
