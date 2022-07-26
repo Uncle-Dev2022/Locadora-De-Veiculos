5e51637338ab14faf47545e6c1a486285e6392fb
nysalvi <73135029+nysalvi@users.noreply.github.com>
2022-07-26 17:31:45 -0300

    Add files via upload

---
+++ Ioc/IServiceLocator.cs
@@ -0,0 +1,9 @@
+﻿using LocadoraDeVeiculos.WindowsFormApp.Compartilhado;
+
+namespace LocadoraDeVeiculos.LocadoraDeVeiculos.WindowsFormApp.Compartilhado.Ioc
+{
+    public interface IServiceLocator
+    {
+        T Get<T>() where T : ControladorBase;
+    }
+}
---
+++ Ioc/ServiceLocatorAutofac.cs
@@ -0,0 +1,19 @@
+﻿using Autofac;
+
+namespace LocadoraDeVeiculos.WindowsFormApp.Compartilhado.Ioc
+{
+    public class ServiceLocatorAutofac
+    {
+        private readonly IContainer container;
+
+        public ServiceLocatorAutofac()
+        {
+            var builder = new ContainerBuilder();
+            container = builder.Build();
+        }
+        public T Get<T>() where T : ControladorBase
+        {
+            return container.Resolve<T>();
+        }
+    }
+}
