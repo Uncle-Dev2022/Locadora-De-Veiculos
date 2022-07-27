38a50b54b930adedc074e1afcc33c74d22ddc17b
User-Wellengton <wellengtonjm@gmail.com>
2022-07-27 16:44:53 -0300

    Add Modulos no ORM

---
+++ LocadoraDeVeiculos.Infra.Orm/ModuloGrupoDeVeiculos/MapeadorGrupoDeVeiculosOrm.cs
@@ -0,0 +1,19 @@
+﻿using LocadoraDeVeiculos.Dominio.ModuloGrupoDeVeiculos;
+using LocadoraDeVeiculos.Infra.Orm.Compartilhado;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+
+namespace LocadoraDeVeiculos.Infra.Orm.ModuloGrupoDeVeiculos
+{
+    public class MapeadorGrupoDeVeiculosOrm : MapeadorBaseOrm<GrupoDeVeiculo>
+    {
+        public override void Configure(EntityTypeBuilder<GrupoDeVeiculo> builder)
+        {
+            builder.ToTable("TBGrupoDeVeiculos");
+            builder.Property(x => x.Id).ValueGeneratedNever().IsRequired();
+            builder.Property(x => x.Nome).HasColumnType("VARCHAR(100)").IsRequired();
+
+        }
+    }
+}
