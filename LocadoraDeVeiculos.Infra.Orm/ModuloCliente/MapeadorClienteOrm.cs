72c031ef4417286bb77f45eae657146f7d74092c
thiago <thiag@NOTETHIAGO>
2022-07-27 16:37:30 -0300

    Mapeador cliente e repositorio cliente

---
+++ LocadoraDeVeiculos.Infra.Orm/ModuloCliente/MapeadorClienteOrm.cs
@@ -0,0 +1,29 @@
+﻿using LocadoraDeVeiculos.Dominio.ModuloCliente;
+using LocadoraDeVeiculos.Infra.Orm.Compartilhado;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocadoraDeVeiculos.Infra.Orm.ModuloCliente
+{
+    public class MapeadorClienteOrm : MapeadorBaseOrm<Cliente>
+    {
+        public override void Configure(EntityTypeBuilder<Cliente> builder)
+        {
+            builder.ToTable("TBCliente");
+            builder.Property(x => x.Id).ValueGeneratedNever().IsRequired();
+            builder.Property(x => x.Nome).HasColumnType("VARCHAR(100)").IsRequired();
+            builder.Property(x => x.CPF_CNPJ).HasColumnType("VARCHAR(50)").IsRequired();
+            builder.Property(x => x.Endereco).HasColumnType("VARCHAR(50)").IsRequired();
+            builder.Property(x => x.Email).HasColumnType("VARCHAR(50)").IsRequired();
+            builder.Property(x => x.Telefone).HasColumnType("VARCHAR(50)").IsRequired();
+            builder.Property(x => x.CNH).HasColumnType("VARCHAR(50)");
+            builder.Property(x => x.tipoCliente);
+            
+        }
+    }
+}
---
+++ LocadoraDeVeiculos.Infra.Orm/ModuloCliente/RepositorioClienteOrm.cs
@@ -0,0 +1,19 @@
+﻿using LocadoraDeVeiculos.Dominio.ModuloCliente;
+using LocadoraDeVeiculos.Infra.Orm.Compartilhado;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocadoraDeVeiculos.Infra.Orm.ModuloCliente
+{
+    public class RepositorioClienteOrm : RepositorioBaseOrm<Cliente,MapeadorClienteOrm>
+    {
+        public RepositorioClienteOrm(LocadoraDeVeiculosDbContext db)
+        {
+            this.db = db;
+            Dados = db.Set<Cliente>();
+        }
+    }
+}
